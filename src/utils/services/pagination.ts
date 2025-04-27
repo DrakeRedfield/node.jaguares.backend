0e25c8ceec45d78862169eaf0fd7cd487aee4099
Drake Redfield <drake.redfield@hotmail.com>
2025-04-27 15:23:07 -0600

    fix: pagination totalpages null

--- src/utils/services/pagination.ts
+++ src/utils/services/pagination.ts
@@ -1,15 +1,15 @@
 import { takeElements } from "../constant/pagination";
 
 export const getPaginationResponse = <T>(data: T[], totalRows: number, page: number) => {
-  const totalPage = Math.ceil(totalRows / takeElements);
-  const nextPage = page + 1 > totalPage ? null : page + 1;
+  const totalPages = Math.ceil(totalRows / takeElements);
+  const nextPage = page + 1 > totalPages ? null : page + 1;
   const prevPage = page - 1 < 1 ? null : page - 1;
   return {
     data,
     pagination: {
       count: totalRows,
       currentPage: page,
-      totalPage,
+      totalPages,
       nextPage,
       prevPage,
     }
